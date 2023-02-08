d3ee42c94cc46df978577ef51b0ae4d93f90c6c5
Michał <miszalaty@gmail.com>
2023-02-08 20:58:09 +0100

    Add project files.

---
+++ TodoList/Controllers/TodoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static IList<TodoModel> todoList = new List<TodoModel>()
+        {
+            new TodoModel(){TaskId = 1, Name = "Make a excercise ", Destriptions = "For interview", ReleaseDate = DateTime.Now}
+        };
+        // GET: TodoController
+        public ActionResult Index()
+        {
+            return View(todoList);
+        }
+
+        // GET: TodoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(todoList.FirstOrDefault(t => t.TaskId == id));
+        }
+
+        // GET: TodoController/Create
+        public ActionResult Create()
+        {
+
+            return View(new TodoModel());
+        }
+
+        // POST: TodoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TodoModel todoModel)
+        {
+            todoModel.TaskId = todoList.Count + 1;
+            todoList.Add(todoModel);
+                return RedirectToAction(nameof(Index));          
+        }
+
+        // GET: TodoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            
+            return View(todoList.FirstOrDefault(t => t.TaskId == id));
+        }
+
+        // POST: TodoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id,TodoModel todoModel)
+        {
+            TodoModel todo = new TodoModel();
+            todo.Name = todoModel.Name;
+            todo.Destriptions = todoModel.Destriptions;
+
+                return RedirectToAction(nameof(Index));
+           
+        }
+
+        // GET: TodoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(todoList.FirstOrDefault(t => t.TaskId == id));
+        }
+
+        // POST: TodoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, TodoModel todoModel)
+        {
+            TodoModel todo = todoList.FirstOrDefault(t => t.TaskId == id);
+            todoList.Remove(todo);
+                return RedirectToAction(nameof(Index));
+            
+        }
+    }
+}
---
+++ TodoList/Models/TodoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TodoModel
+    {
+        public int TaskId { get; set; }
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [DisplayName("Destription")]
+        public string Destriptions { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+}
